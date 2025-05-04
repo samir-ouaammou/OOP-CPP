d7dcd3934a16f13aa1975b4ad04e0419fb150ff0
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2025-05-04 10:58:38 +0100

    finish ex02 HI THIS IS BRAIN

---
+++ C++ Module 01/ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int	main(void)
+{
+	std::string	MyString = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &MyString;
+	std::string	&stringREF = MyString;
+
+	std::cout << "Memory address of myString:   " << &MyString << std::endl;
+    std::cout << "Memory address held by PTR:   " << stringPTR << std::endl;
+    std::cout << "Memory address held by REF:   " << &stringREF << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Value of myString:            " << MyString << std::endl;
+    std::cout << "Value pointed to by PTR:      " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by REF:      " << stringREF << std::endl;
+
+	return (0);
+}
