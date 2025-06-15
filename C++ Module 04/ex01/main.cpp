bd72e0f7f686247af7911128975dd91c92bdfc97
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2025-06-15 22:33:16 +0100

    finish ex01 in C++ Module 04

--- C++ Module 04/ex01/main.cpp
+++ C++ Module 04/ex01/main.cpp
@@ -1,12 +1,17 @@
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+#include "Brain.hpp"
 
 int main()
 {
     const Animal* j = new Dog();
     const Animal* i = new Cat();
+
     delete j;
     delete i;
+
     return (0);
 }
