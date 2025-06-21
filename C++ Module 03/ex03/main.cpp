a1edef991f489d29fad7124adab9fd7e2c84d8c4
Samir Ouaammou <souaammo@c3r10p8.1337.ma>
2025-06-21 18:50:09 +0100

    finish C++ 03

--- C++ Module 03/ex03/main.cpp
+++ C++ Module 03/ex03/main.cpp
@@ -1,17 +1,18 @@
 #include "DiamondTrap.hpp"
 #include <iostream>
 
-int main() {
+int main()
+{
     std::cout << "\n=== Creating DiamondTrap ===" << std::endl;
     DiamondTrap dt("Samir");
 
     std::cout << "\n=== Testing actions ===" << std::endl;
-    dt.attack("target_1");         // من ScavTrap
-    dt.takeDamage(20);             // من ClapTrap
-    dt.beRepaired(10);             // من ClapTrap
-    dt.guardGate();                // من ScavTrap
-    dt.highFivesGuys();            // من FragTrap
-    dt.whoAmI();                   // خاص بـ DiamondTrap
+    dt.attack("target_1");         //  ScavTrap
+    dt.takeDamage(20);             //  ClapTrap
+    dt.beRepaired(10);             //  ClapTrap
+    dt.guardGate();                //  ScavTrap
+    dt.highFivesGuys();            //  FragTrap
+    dt.whoAmI();                   //  DiamondTrap
 
     std::cout << "\n=== Destructor will be called automatically ===" << std::endl;
     return 0;
