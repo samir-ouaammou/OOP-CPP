a97d010b04284e57a5010ae1fe364df89907e620
Samir Ouaammou <souaammo@c3r9p6.1337.ma>
2025-07-05 18:19:46 +0100

    update ex00 in C++ Module 05

--- C++ Module 05/ex00/Bureaucrat.hpp
+++ C++ Module 05/ex00/Bureaucrat.hpp
@@ -26,13 +26,13 @@ class Bureaucrat
         class GradeTooHighException : public std::exception
         {
             public:
-                virtual const char* what() const throw();
+                const char* what() const throw();
         };
 
         class GradeTooLowException : public std::exception
         {
             public:
-                virtual const char* what() const throw();
+                const char* what() const throw();
         };
 };
 
