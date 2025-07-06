2c4841c0d3f18215aa108cccb0a4d07d333d7f0a
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2025-07-06 14:56:53 +0100

    add ex02

---
+++ C++ Module 05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,20 @@
+#ifndef PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm
+{
+    private:
+        std::string _target;
+
+    public:
+        PresidentialPardonForm(const std::string &target);
+        PresidentialPardonForm(const PresidentialPardonForm &copy);
+        PresidentialPardonForm &operator=(const PresidentialPardonForm &src);
+        ~PresidentialPardonForm();
+
+        void execute(Bureaucrat const &executor) const;
+};
+
+#endif
