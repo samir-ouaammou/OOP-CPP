23ff1b3d362fbfddceca5b4cb45c42ecf06c9b01
Samir Ouaammou <souaammo@c3r9p6.1337.ma>
2025-07-12 18:25:53 +0100

    update ex01 in c++ 07

--- C++ Module 07/ex01/iter.hpp
+++ C++ Module 07/ex01/iter.hpp
@@ -13,7 +13,7 @@ void    printElement(const T &elem)
 template <typename T>
 void    iter(T *array, size_t length, void (*func)(T const &))
 {
-    for (size_t i = 0; i < length; ++i)
+    for (size_t i = 0; i < length; i++)
         func(array[i]);
 }
 
--- C++ Module 07/ex01/main.cpp
+++ C++ Module 07/ex01/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     int arrInt[5] = {1, 2, 3, 4, 5};
-    std::string arrStr[3] = {"cpp", "module", "07"};
+    std::string arrStr[3] = {"Samir", "Ouaammou", "1337"};
 
     std::cout << "Int array:" << std::endl;
     iter(arrInt, 5, printElement);
@@ -11,9 +11,5 @@ int main()
     std::cout << "String array:" << std::endl;
     iter(arrStr, 3, printElement);
 
-    double arrDouble[4] = {1.1, 2.2, 3.3, 4.4};
-    std::cout << "Double array:" << std::endl;
-    iter(arrDouble, 4, printElement);
-
-    return 0;
+    return (0);
 }
