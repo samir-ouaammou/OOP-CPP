0c67799c51a1e217a8a5884c23c1cb4e7f46ebff
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2025-07-12 16:00:48 +0100

    add ex00 in C++  Module 07

--- C++ Module 07/ex00/main.cpp
+++ C++ Module 07/ex00/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <string>
 #include "whatever.hpp"
 
 int main( void )
--- C++ Module 07/ex00/whatever.hpp
+++ C++ Module 07/ex00/whatever.hpp
@@ -1,8 +1,10 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-template <typename T>
+#include <iostream>
+#include <string>
 
+template <typename T>
 void    swap(T &a, T &b)
 {
     T tmp = a;
@@ -11,14 +13,12 @@ void    swap(T &a, T &b)
 }
 
 template <typename T>
-
 T const &min(T const &a, T const &b)
 {
     return (a < b ? a : b);
 }
 
 template <typename T>
-
 T const &max(T const &a, T const &b)
 {
     return (a > b ? a : b);
