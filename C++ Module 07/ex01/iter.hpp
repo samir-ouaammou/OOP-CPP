1aa5e375a6215122fff7f657ad78dfa98d521342
Samir Ouaammou <souaammo@c3r10p8.1337.ma>
2025-07-14 16:05:34 +0100

    finish CPP07

--- C++ Module 07/ex01/iter.hpp
+++ C++ Module 07/ex01/iter.hpp
@@ -5,13 +5,20 @@
 #include <string>
 
 template <typename T>
-void    printElement(T &elem)
+void    printElement(const T &elem)
 {
     std::cout << elem << std::endl;
 }
 
 template <typename T>
 void    iter(T array[], size_t length, void (*func)(T &))
+{
+    for (size_t i = 0; i < length; i++)
+        func(array[i]);
+}
+
+template <typename T>
+void    iter(T array[], size_t length, void (*func)(const T &))
 {
     for (size_t i = 0; i < length; i++)
         func(array[i]);
