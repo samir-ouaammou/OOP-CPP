bd0e807b0cf887975d0ab289865dcad1193c1a79
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2025-07-16 14:36:58 +0100

    update ex00 in C++ Module 08

--- C++ Module 08/ex00/easyfind.hpp
+++ C++ Module 08/ex00/easyfind.hpp
@@ -12,7 +12,7 @@ typename    T::iterator easyfind(T &container, int value)
 {
     typename    T::iterator it = std::find(container.begin(), container.end(), value);
     if (it == container.end())
-        throw std::runtime_error("Value not found in container");
+        throw std::out_of_range("Value not found in container");
     
     return (it);
 }
