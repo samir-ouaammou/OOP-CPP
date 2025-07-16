9046d4c96c96ae0667f30989fba578f37ae24fa5
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2025-07-16 11:20:04 +0100

    add C++  Module 08

---
+++ C++ Module 08/ex00/main.cpp
@@ -0,0 +1,99 @@
+#include "easyfind.hpp"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+
+int main()
+{
+    std::cout << "<----------Testing easyfind with std::vector<int>---------->\n" << std::endl;
+    std::vector<int>    Vector;
+
+    Vector.push_back(1);
+    Vector.push_back(2);
+    Vector.push_back(3);
+    Vector.push_back(4);
+    Vector.push_back(5);
+
+    try
+    {
+        std::vector<int>::iterator it = easyfind(Vector, 3);
+        std::cout << "Found: " << *it << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    try
+    {
+        std::vector<int>::iterator it = easyfind(Vector, 6);
+        std::cout << "Found: " << *it << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::cout << "\n<----------Testing easyfind with std::list<int>---------->\n" << std::endl;
+
+    std::list<int>    List;
+
+    List.push_back(10);
+    List.push_back(20);
+    List.push_back(30);
+    List.push_back(40);
+    List.push_back(50);
+
+    try
+    {
+        std::list<int>::iterator it = easyfind(List, 30);
+        std::cout << "Found: " << *it << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    try
+    {
+        std::list<int>::iterator it = easyfind(List, 6);
+        std::cout << "Found: " << *it << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::cout << "<----------Testing easyfind with std::set<int>---------->\n" << std::endl;
+
+    std::set<int>    Set;
+
+    Set.insert(-1);
+    Set.insert(-2);
+    Set.insert(-3);
+    Set.insert(-4);
+    Set.insert(-5);
+
+    try
+    {
+        std::set<int>::iterator it = easyfind(Set, -3);
+        std::cout << "Found: " << *it << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    try
+    {
+        std::set<int>::iterator it = easyfind(Set, -6);
+        std::cout << "Found: " << *it << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
