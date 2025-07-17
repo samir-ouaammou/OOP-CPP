c5eebdf9e660101dcdd79f1d5cece73badf5c172
Samir Ouaammou <souaammo@c3r9p5.1337.ma>
2025-07-17 14:34:23 +0100

    update ex01 in c++ 08

--- C++ Module 08/ex01/main.cpp
+++ C++ Module 08/ex01/main.cpp
@@ -1,16 +1,39 @@
-#include "Span.hpp"
 #include <iostream>
-
+#include <vector>
+#include "Span.hpp"
 
 int main()
 {
-    Span sp = Span(5);
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    try
+    {
+        Span sp(10);
+        std::vector<int> moreNumbers;
+
+        sp.addNumber(5);
+        sp.addNumber(3);
+        sp.addNumber(9);
+
+        moreNumbers.push_back(1);
+        moreNumbers.push_back(7);
+        moreNumbers.push_back(10);
+        moreNumbers.push_back(2);
+
+        sp.addRange(moreNumbers.begin(), moreNumbers.end());
+
+        std::cout << "Shortest Span: " << sp.shortestSpan() << std::endl;
+        std::cout << "Longest Span: " << sp.longestSpan() << std::endl;
+
+        sp.addNumber(6);
+        sp.addNumber(8);
+        sp.addNumber(15);
+
+        sp.addNumber(42);
+
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+
     return 0;
 }
